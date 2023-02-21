e3ef12453bfdc9e4953e73a2c3a41c86d0abb1e7
GhostVaibhav <48472541+GhostVaibhav@users.noreply.github.com>
2023-02-21 21:36:30 +0530

    Initial Commit

---
+++ HeadAudioSpatializer.cpp
@@ -0,0 +1,12 @@
+﻿// HeadAudioSpatializer.cpp : Defines the entry point for the application.
+//
+
+#include "HeadAudioSpatializer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
